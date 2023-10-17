1ad5cc49177d771c851eb49a91440c9b446e735c
= <60821161+Agua-bit@users.noreply.github.com>
2023-10-17 00:01:25 -0500

    Update Portero.java

--- src/main/java/Portero.java
+++ src/main/java/Portero.java
@@ -5,8 +5,8 @@ public class Portero extends Futbolista{
     public short golesRecibidos;
     public byte dorsal;
 
-    public Portero(String nombre, int edad, String posicion, short golesRecibidos, byte dorsal) {
-        super(nombre, edad, posicion);
+    public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
+        super(nombre, edad, "Portero");
         this.golesRecibidos = golesRecibidos;
         this.dorsal = dorsal;
     }
@@ -15,5 +15,20 @@ public class Portero extends Futbolista{
     public String toString() {
         return  "El futbolista " + this.getNombre() + " tiene " + this.getEdad() + ", y juega de " + this.getPosicion() +
                 " con el dorsal " + this.dorsal + ". Le han marcado " + this.golesMarcados;
+    }
+
+    @Override
+    public boolean juegaConLasManos(){
+        return true;
+    }
+
+    @Override
+    public int compareTo(Object e) {
+        if (e instanceof Portero){
+            Portero portero = (Portero) e;
+            return Math.abs(this.golesRecibidos - portero.golesRecibidos)
+        } else {
+            return 0;
+        }
     }
 }
